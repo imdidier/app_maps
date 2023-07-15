2a41e29bb3a2e3788392ad447dabdaa0cd7384a1
Didier David <71243788+imdidierjunco@users.noreply.github.com>
2023-07-15 13:53:54 -0500

    FEAT: Add recovery_password view.

---
+++ lib/ui/views/recovery_password_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../widgets/widgets.dart';
+
+class RecoveryPasswordView extends StatelessWidget {
+  const RecoveryPasswordView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recuperar contraseña'),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            context.go('/');
+          },
+          icon: const Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LogoWidget(),
+              CustomTextInput(
+                obscureText: true,
+                hintText: 'Nueva contraseña',
+                label: 'Nueva contraseña',
+              ),
+              SizedBox(height: 20),
+              CustomTextInput(
+                obscureText: true,
+                hintText: 'Repita contraseña',
+                label: 'Repetir contraseña',
+              ),
+              Expanded(child: SizedBox()),
+              CustomButton(
+                title: 'Cambiar contraseña',
+                urlRuta: '/home',
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
