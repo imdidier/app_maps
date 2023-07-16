954a5bb3648250ab9c93d69342f3815354b328ae
Didier David <71243788+imdidierjunco@users.noreply.github.com>
2023-07-16 15:44:21 -0500

    FEAT: Start clean architecture to manipulate user data.

--- lib/ui/providers/sing_in_provider.dart
+++ lib/ui/providers/sing_in_provider.dart
@@ -1,12 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 import '../../firebase_options.dart';
 
 class SingInProvider extends ChangeNotifier {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  User? get currentUser => firebaseAuth.currentUser;
+
   initFirebase() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+  }
+
+  Future<void> singInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      await firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      if (kDebugMode) {
+        print('SinInProvider, singInWithEmailAndPassword, Error: $e');
+      }
+    }
+  }
+
+  Future<void> createUserWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      await firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      if (kDebugMode) {
+        print('SinInProvider, createUserWithEmailAndPassword, Error: $e');
+      }
+    }
+  }
+
+  Future<void> singOut() async {
+    try {
+      await firebaseAuth.signOut();
+    } catch (e) {
+      if (kDebugMode) {
+        print('SinInProvider, singOut, Error: $e');
+      }
+    }
   }
 }
